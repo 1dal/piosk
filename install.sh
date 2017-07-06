f10a10923e27206118bccae05ea5dca5ba3e7ff1
zoutepopcorn <zoutepopcorn@hotmail.com>
2017-07-06 07:29:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo systemctl disable getty@tty1
 sudo apt-get install fbi
 sudo wget /etc/http://www.pngmart.com/files/3/Chocolate-Splash-PNG-Pic.png
 sudo cp /etc/Chocolate-Splash-PNG-Pic.png splash.png
+sudo apt-get --no-install-recommends install xserver-xorg xserver-xorg-video-fbdev xinit pciutils xinput xfonts-100dpi xfonts-75dpi xfonts-scalable
