1c812e993620d4643a1986c469aff07c6dc97503
zoutepopcorn <zoutepopcorn@hotmail.com>
2017-07-06 07:28:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-#/bin/bash
+#!/bin/bash
 sudo apt-get purge --remove plymouth
 sudo systemctl disable getty@tty1
 sudo apt-get install fbi
-
 sudo wget /etc/http://www.pngmart.com/files/3/Chocolate-Splash-PNG-Pic.png
 sudo cp /etc/Chocolate-Splash-PNG-Pic.png splash.png
